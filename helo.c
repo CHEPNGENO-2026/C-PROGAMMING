d165d780ba19819c878668ba1bf5c670363cb7a1
CHEPNGENO-2026 <beckychepngeno54@gmail.com>
2026-05-18 09:06:08 +0300

    Add files via upload

---
+++ helo.c
@@ -0,0 +1,23 @@
+// Program that displays all the academic units offered this semester
+
+/*
+Name:Bett Chepngeno
+Reg:BCS-03-0065/2026
+Date:15-05-2026
+*/
+
+#include<stdio.h>
+
+int main() {
+
+	printf("UNITS OFFERED THIS SEMESTER;\n");
+	printf("1.Communication skills\n");
+	printf("2.Digital litracy \n");
+	printf("3.Health and wellness management");
+	printf("4.Data ethics,privacy and governance \n");
+	printf("5.Structured programming and algorithms \n");
+	printf("6.Basic Mathematics \n");
+	
+	return 0;
+}
+   
